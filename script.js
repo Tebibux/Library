61b8f62b039b8aeeb8be1fc2e81f0259c9c42a0a
tebibux <tebibusolomon79@gmail.com>
2022-10-26 21:54:30 +0300

    adding to the dashbord done

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // local library array to store data 
 let myLibrary = [];
+// Call the HTML book shelf element as Shelf
+const shelf = document.querySelector('.main-left');
+// calling the button
+const subBtn = document.querySelector('#button')
 
 function Book(title, author, page, date, comment) {
 	// the constructor...
@@ -19,4 +23,39 @@ addBookToLibrary('T-she Hussien', 'Bogale Teferi', 195, 1990, 'scarey book');
 addBookToLibrary('Yoratorad', 'Yismake', 100, 1998, 'Fantacy');
 addBookToLibrary('Tebegna Ewnetoch', 'Betty', 200, 2000, 'critics book');
 
-console.table(myLibrary);
+myLibrary.forEach(books => {
+	let book = document.createElement('div');
+	book.className = 'book';
+	shelf.append(book);
+	// creating unordered list
+	let uList = document.createElement('ul');
+	book.append(uList);
+	// creating list Items
+	// title
+	let tPara = document.createElement('li');
+	tPara.innerText = `Title: ${books.title}`;
+	uList.append(tPara);
+	// author
+	let aPara = document.createElement('li');
+	aPara.innerText = `Author: ${books.author}`;
+	uList.append(aPara);
+
+	// page
+	let pPara = document.createElement('li');
+	pPara.innerText = `Page: ${books.page}`;
+	uList.append(pPara);
+	// date
+	let dPara = document.createElement('li');
+	dPara.innerText = `Date: ${books.date}`;
+	uList.append(dPara);
+	// comment
+	let cPara = document.createElement('li');
+	cPara.innerText = `Comment: ${books.comment}`
+	uList.append(cPara);
+	
+	// console.table(books.author);
+})
+
+
+
+// console.table(myLibrary);
